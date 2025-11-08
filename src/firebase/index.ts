69da0b4a5a3ad0212fcbbd502163a6150a6e3011
shaaryan <shaaryannnn@gmail.com>
2025-11-08 07:43:28 +0000

    Try fixing this error: `Runtime Error: Error: useMemo is not defined. Er

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -2,6 +2,7 @@ import { initializeApp, getApp, getApps, type FirebaseApp } from 'firebase/app';
 import { getAuth, type Auth } from 'firebase/auth';
 import { getFirestore, type Firestore } from 'firebase/firestore';
 import { firebaseConfig } from './config';
+import { useMemo } from 'react';
 
 import { FirebaseProvider, useFirebase, useFirebaseApp, useFirestore, useAuth } from './provider';
 import { FirebaseClientProvider } from './client-provider';
